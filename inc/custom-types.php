0d4136f94fd108bf8968e43581854a500a90894f
woodwardtw <twwoodward@me.com>
2021-09-16 17:03:35 -0400

    tools cpt

--- inc/custom-types.php
+++ inc/custom-types.php
@@ -9,44 +9,44 @@
 defined( 'ABSPATH' ) || exit;
 
 
-//resource custom post type
+//tool custom post type
 
-// Register Custom Post Type resource
-// Post Type Key: resource
+// Register Custom Post Type tool
+// Post Type Key: tool
 
-function create_resource_cpt() {
+function create_tool_cpt() {
 
   $labels = array(
-    'name' => __( 'Resources', 'Post Type General Name', 'textdomain' ),
-    'singular_name' => __( 'Resource', 'Post Type Singular Name', 'textdomain' ),
-    'menu_name' => __( 'Resource', 'textdomain' ),
-    'name_admin_bar' => __( 'Resource', 'textdomain' ),
-    'archives' => __( 'Resource Archives', 'textdomain' ),
-    'attributes' => __( 'Resource Attributes', 'textdomain' ),
-    'parent_item_colon' => __( 'Resource:', 'textdomain' ),
-    'all_items' => __( 'All Resources', 'textdomain' ),
-    'add_new_item' => __( 'Add New Resource', 'textdomain' ),
+    'name' => __( 'Tools', 'Post Type General Name', 'textdomain' ),
+    'singular_name' => __( 'Tool', 'Post Type Singular Name', 'textdomain' ),
+    'menu_name' => __( 'Tool', 'textdomain' ),
+    'name_admin_bar' => __( 'Tool', 'textdomain' ),
+    'archives' => __( 'Tool Archives', 'textdomain' ),
+    'attributes' => __( 'Tool Attributes', 'textdomain' ),
+    'parent_item_colon' => __( 'Tool:', 'textdomain' ),
+    'all_items' => __( 'All Tools', 'textdomain' ),
+    'add_new_item' => __( 'Add New Tool', 'textdomain' ),
     'add_new' => __( 'Add New', 'textdomain' ),
-    'new_item' => __( 'New Resource', 'textdomain' ),
-    'edit_item' => __( 'Edit Resource', 'textdomain' ),
-    'update_item' => __( 'Update Resource', 'textdomain' ),
-    'view_item' => __( 'View Resource', 'textdomain' ),
-    'view_items' => __( 'View Resources', 'textdomain' ),
-    'search_items' => __( 'Search Resources', 'textdomain' ),
+    'new_item' => __( 'New Tool', 'textdomain' ),
+    'edit_item' => __( 'Edit Tool', 'textdomain' ),
+    'update_item' => __( 'Update Tool', 'textdomain' ),
+    'view_item' => __( 'View Tool', 'textdomain' ),
+    'view_items' => __( 'View Tools', 'textdomain' ),
+    'search_items' => __( 'Search Tools', 'textdomain' ),
     'not_found' => __( 'Not found', 'textdomain' ),
     'not_found_in_trash' => __( 'Not found in Trash', 'textdomain' ),
     'featured_image' => __( 'Featured Image', 'textdomain' ),
     'set_featured_image' => __( 'Set featured image', 'textdomain' ),
     'remove_featured_image' => __( 'Remove featured image', 'textdomain' ),
     'use_featured_image' => __( 'Use as featured image', 'textdomain' ),
-    'insert_into_item' => __( 'Insert into resource', 'textdomain' ),
-    'uploaded_to_this_item' => __( 'Uploaded to this resource', 'textdomain' ),
-    'items_list' => __( 'Resource list', 'textdomain' ),
-    'items_list_navigation' => __( 'Resource list navigation', 'textdomain' ),
-    'filter_items_list' => __( 'Filter Resource list', 'textdomain' ),
+    'insert_into_item' => __( 'Insert into tool', 'textdomain' ),
+    'uploaded_to_this_item' => __( 'Uploaded to this tool', 'textdomain' ),
+    'items_list' => __( 'Tool list', 'textdomain' ),
+    'items_list_navigation' => __( 'Tool list navigation', 'textdomain' ),
+    'filter_items_list' => __( 'Filter Tool list', 'textdomain' ),
   );
   $args = array(
-    'label' => __( 'resource', 'textdomain' ),
+    'label' => __( 'tool', 'textdomain' ),
     'description' => __( '', 'textdomain' ),
     'labels' => $labels,
     'menu_icon' => '',
@@ -65,121 +65,12 @@ function create_resource_cpt() {
     'show_in_rest' => true,
     'publicly_queryable' => true,
     'capability_type' => 'post',
-    'menu_icon' => 'dashicons-universal-access-alt',
+    'menu_icon' => 'dashicons-admin-tools',
   );
-  register_post_type( 'resource', $args );
+  register_post_type( 'tool', $args );
   
   // flush rewrite rules because we changed the permalink structure
   global $wp_rewrite;
   $wp_rewrite->flush_rules();
 }
-add_action( 'init', 'create_resource_cpt', 0 );
-
-add_action( 'init', 'create_objective_taxonomies', 0 );
-function create_objective_taxonomies()
-{
-  // Add new taxonomy, NOT hierarchical (like tags)
-  $labels = array(
-    'name' => _x( 'Objectives', 'taxonomy general name' ),
-    'singular_name' => _x( 'Objective', 'taxonomy singular name' ),
-    'search_items' =>  __( 'Search Objectives' ),
-    'popular_items' => __( 'Popular Objectives' ),
-    'all_items' => __( 'All Objectives' ),
-    'parent_item' => null,
-    'parent_item_colon' => null,
-    'edit_item' => __( 'Edit Objectives' ),
-    'update_item' => __( 'Update Objective' ),
-    'add_new_item' => __( 'Add New Objective' ),
-    'new_item_name' => __( 'New Objective' ),
-    'add_or_remove_items' => __( 'Add or remove Objectives' ),
-    'choose_from_most_used' => __( 'Choose from the most used Objectives' ),
-    'menu_name' => __( 'Objectives' ),
-  );
-
-//registers taxonomy specific post types - default is just post
-  register_taxonomy('Objectives',array('resource'), array(
-    'hierarchical' => true,
-    'labels' => $labels,
-    'show_ui' => true,
-    'update_count_callback' => '_update_post_term_count',
-    'query_var' => true,
-    'rewrite' => array( 'slug' => 'objective' ),
-    'show_in_rest'          => true,
-    'rest_base'             => 'objective',
-    'rest_controller_class' => 'WP_REST_Terms_Controller',
-    'show_in_nav_menus' => false,    
-  ));
-}
-
-add_action( 'init', 'create_software_taxonomies', 0 );
-function create_software_taxonomies()
-{
-  // Add new taxonomy, NOT hierarchical (like tags)
-  $labels = array(
-    'name' => _x( 'Software', 'taxonomy general name' ),
-    'singular_name' => _x( 'software', 'taxonomy singular name' ),
-    'search_items' =>  __( 'Search Software' ),
-    'popular_items' => __( 'Popular Software' ),
-    'all_items' => __( 'All Software' ),
-    'parent_item' => null,
-    'parent_item_colon' => null,
-    'edit_item' => __( 'Edit Software' ),
-    'update_item' => __( 'Update software' ),
-    'add_new_item' => __( 'Add New software' ),
-    'new_item_name' => __( 'New software' ),
-    'add_or_remove_items' => __( 'Add or remove Software' ),
-    'choose_from_most_used' => __( 'Choose from the most used Software' ),
-    'menu_name' => __( 'Software' ),
-  );
-
-//registers taxonomy specific post types - default is just post
-  register_taxonomy('Software',array('resource'), array(
-    'hierarchical' => true,
-    'labels' => $labels,
-    'show_ui' => true,
-    'update_count_callback' => '_update_post_term_count',
-    'query_var' => true,
-    'rewrite' => array( 'slug' => 'software' ),
-    'show_in_rest'          => true,
-    'rest_base'             => 'software',
-    'rest_controller_class' => 'WP_REST_Terms_Controller',
-    'show_in_nav_menus' => false,    
-  ));
-}
-
-add_action( 'init', 'create_course_taxonomies', 0 );
-function create_course_taxonomies()
-{
-  // Add new taxonomy, NOT hierarchical (like tags)
-  $labels = array(
-    'name' => _x( 'Courses', 'taxonomy general name' ),
-    'singular_name' => _x( 'course', 'taxonomy singular name' ),
-    'search_items' =>  __( 'Search Courses' ),
-    'popular_items' => __( 'Popular Courses' ),
-    'all_items' => __( 'All Courses' ),
-    'parent_item' => null,
-    'parent_item_colon' => null,
-    'edit_item' => __( 'Edit Courses' ),
-    'update_item' => __( 'Update course' ),
-    'add_new_item' => __( 'Add New course' ),
-    'new_item_name' => __( 'New course' ),
-    'add_or_remove_items' => __( 'Add or remove Courses' ),
-    'choose_from_most_used' => __( 'Choose from the most used Courses' ),
-    'menu_name' => __( 'Courses' ),
-  );
-
-//registers taxonomy specific post types - default is just post
-  register_taxonomy('Courses',array('resource'), array(
-    'hierarchical' => true,
-    'labels' => $labels,
-    'show_ui' => true,
-    'update_count_callback' => '_update_post_term_count',
-    'query_var' => true,
-    'rewrite' => array( 'slug' => 'course' ),
-    'show_in_rest'          => true,
-    'rest_base'             => 'course',
-    'rest_controller_class' => 'WP_REST_Terms_Controller',
-    'show_in_nav_menus' => false,    
-  ));
-}
-
+add_action( 'init', 'create_tool_cpt', 0 );
