c2a9e63f6d193582ec09ea4c3a7ac225ba4028c5
woodwardtw <twwoodward@me.com>
2021-09-17 07:53:45 -0400

    add maker cpt

--- inc/custom-types.php
+++ inc/custom-types.php
@@ -74,3 +74,71 @@ function create_tool_cpt() {
   $wp_rewrite->flush_rules();
 }
 add_action( 'init', 'create_tool_cpt', 0 );
+
+
+
+//maker custom post type
+
+// Register Custom Post Type maker
+// Post Type Key: maker
+
+function create_maker_cpt() {
+
+  $labels = array(
+    'name' => __( 'Makers', 'Post Type General Name', 'textdomain' ),
+    'singular_name' => __( 'Maker', 'Post Type Singular Name', 'textdomain' ),
+    'menu_name' => __( 'Maker', 'textdomain' ),
+    'name_admin_bar' => __( 'Maker', 'textdomain' ),
+    'archives' => __( 'Maker Archives', 'textdomain' ),
+    'attributes' => __( 'Maker Attributes', 'textdomain' ),
+    'parent_item_colon' => __( 'Maker:', 'textdomain' ),
+    'all_items' => __( 'All Makers', 'textdomain' ),
+    'add_new_item' => __( 'Add New Maker', 'textdomain' ),
+    'add_new' => __( 'Add New', 'textdomain' ),
+    'new_item' => __( 'New Maker', 'textdomain' ),
+    'edit_item' => __( 'Edit Maker', 'textdomain' ),
+    'update_item' => __( 'Update Maker', 'textdomain' ),
+    'view_item' => __( 'View Maker', 'textdomain' ),
+    'view_items' => __( 'View Makers', 'textdomain' ),
+    'search_items' => __( 'Search Makers', 'textdomain' ),
+    'not_found' => __( 'Not found', 'textdomain' ),
+    'not_found_in_trash' => __( 'Not found in Trash', 'textdomain' ),
+    'featured_image' => __( 'Featured Image', 'textdomain' ),
+    'set_featured_image' => __( 'Set featured image', 'textdomain' ),
+    'remove_featured_image' => __( 'Remove featured image', 'textdomain' ),
+    'use_featured_image' => __( 'Use as featured image', 'textdomain' ),
+    'insert_into_item' => __( 'Insert into maker', 'textdomain' ),
+    'uploaded_to_this_item' => __( 'Uploaded to this maker', 'textdomain' ),
+    'items_list' => __( 'Maker list', 'textdomain' ),
+    'items_list_navigation' => __( 'Maker list navigation', 'textdomain' ),
+    'filter_items_list' => __( 'Filter Maker list', 'textdomain' ),
+  );
+  $args = array(
+    'label' => __( 'maker', 'textdomain' ),
+    'description' => __( '', 'textdomain' ),
+    'labels' => $labels,
+    'menu_icon' => '',
+    'supports' => array('title', 'editor', 'revisions', 'author', 'trackbacks', 'custom-fields', 'thumbnail',),
+    'taxonomies' => array('category', 'post_tag'),
+    'public' => true,
+    'show_ui' => true,
+    'show_in_menu' => true,
+    'menu_position' => 5,
+    'show_in_admin_bar' => true,
+    'show_in_nav_menus' => true,
+    'can_export' => true,
+    'has_archive' => true,
+    'hierarchical' => false,
+    'exclude_from_search' => false,
+    'show_in_rest' => true,
+    'publicly_queryable' => true,
+    'capability_type' => 'post',
+    'menu_icon' => 'dashicons-universal-access-alt',
+  );
+  register_post_type( 'maker', $args );
+  
+  // flush rewrite rules because we changed the permalink structure
+  global $wp_rewrite;
+  $wp_rewrite->flush_rules();
+}
+add_action( 'init', 'create_maker_cpt', 0 );
